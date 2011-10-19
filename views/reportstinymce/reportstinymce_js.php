8b24f70f76270dbb5fe8c19c51712e02b2a7cd79
John Etherton <john.etherton@gmail.com>
2011-10-19 14:48:54 -0600

    Fixed it so that the html formatting is correctly saved.

--- views/reportstinymce/reportstinymce_js.php
+++ views/reportstinymce/reportstinymce_js.php
@@ -11,22 +11,19 @@
 <script type="text/javascript" src="<?php echo url::base(); ?>/media/js/tinymce/tiny_mce.js"></script>
 
 <script type="text/javascript" charset="utf-8">
-// Initialize tinyMCE Wysiwyg Editor
+//Initialize tinyMCE Wysiwyg Editor
 tinyMCE.init({
-	mode : "exact",
-	elements : "incident_description",
-	theme : "advanced",
-	theme_advanced_buttons1 : "mybutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,code",
-	theme_advanced_buttons2 : "outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,code,|,forecolor,backcolor,|,cut,copy,paste,pastetext,pasteword,|,hr,visualaid,|,sub,sup",
-	theme_advanced_buttons3 : "",
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_statusbar_location : "bottom",
-	toolbar_location : "top",
-	height:"400px",
-	width:"<?php echo $width;?>px",
-	encoding : "xml",
-	entity_encoding : "named",
-	extended_valid_elements : "iframe[src|width|height|name|align|frameborder|scrolling]"
+convert_urls : false,
+relative_urls : false,
+mode : "exact",
+elements : "incident_description",
+theme : "advanced",
+plugins : "pagebreak,advhr,advimage,advlink,iespell,inlinepopups,contextmenu,paste,directionality,noneditable,advlist",
+// Theme options
+theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,fontselect,fontsizeselect",
+theme_advanced_buttons2 : "outdent,indent,blockquote,|,undo,redo,|,link,unlink,image,code,|,forecolor,backcolor",
+theme_advanced_buttons3 : "cut,copy,paste,pastetext,pasteword,|,hr,removeformat,visualaid,|,sub,sup,|,advhr,|,ltr,rtl",
+theme_advanced_toolbar_location : "top",
+theme_advanced_toolbar_align : "left"
 });
 </script>
